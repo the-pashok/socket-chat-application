df15572bf16a2746b44e0107c50f91292dc13a38
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-07-21 14:59:39 +0300

    fix send messages error

--- public/functions.js
+++ public/functions.js
@@ -4,21 +4,19 @@ let name = document.getElementById('name');
 let messages = document.getElementById('messages');
 let online = document.getElementById('online');
 
-
 function sendMess(event) {
     event.preventDefault();
-
-    if(textarea.value.length !== 0 && textarea.value !== '' && name.value.length > 0 && name.value !== '') {
+    if (textarea.value.length !== 0 && textarea.value !== '' && name.value.length > 0 && name.value !== '') {
         socket.emit('send message', {message: textarea.value, name: name.value});
     }
-
     textarea.value = '';
+    return false;
 }
 
 socket.on('add message', data => {
     messages.innerHTML += "<div class='message-item'><b>" + data.name + " : </b>" + data.message + "</div>";
 });
 
-socket.on('getPeopleOnline', length => {
+socket.on('getPeopleOnline', function (length) {
     online.innerText = 'Online: ' + length;
 });
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const socketio = require('socket.io')(server);
+const http = require('http').createServer(app);
+const io = require('socket.io').listen(http);
+const socketio = require('socket.io-client');
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -10,26 +12,26 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
 
+http.listen(port, () => {
+    console.log(`Server is running... ${port}`);
+});
+
 let connections = [];
 
-socketio.sockets.on('connection', (socket) => {
+io.sockets.on('connection', (socket) => {
     // when someone connects to your server, add socket object to array
     connections.push(socket);
     console.log('Connected!');
 
-    socket.on('disconnect', (data) => {
+    socket.on('disconnect', data => {
         socket.emit('getPeopleOnline', connections.length);
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected!');
     });
 
     socket.on('send message', data => {
-        socket.emit('add message', {message: data.message, name: data.name});
+        io.sockets.emit('add message', {name: data.name, message: data.message});
     });
 
     socket.emit('getPeopleOnline', connections.length);
-});
-
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port 3000 or ${process.env.PORT}`);
 });
